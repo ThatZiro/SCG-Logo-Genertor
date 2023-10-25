e78a15afc316e06b27bbccb9cb66331decd55212
ImBrandonBarnes <ImBrandonBarnes@gmail.com>
2023-10-24 19:33:09 -0700

    Added Unit Test

--- index.js
+++ index.js
@@ -1,53 +1,16 @@
 const fs = require("fs");
 const inquirer = require('inquirer');
+const {Triangle, Square, Circle} = require('./lib/shapes');
 
-class Shape{
-    constructor(_data){
-        this.data = _data;
-    }
-    innerHTML(){
-        let dy = ".3em";
-        if(this.data.shape == "Triangle"){
-          dy = "1em"  
-        }
-
-        return`
-        <svg width="100" height="100">
-            ${this.render()}
-            <text x="50%" y="50%" font-size="24px" font-weight="bold" text-anchor="middle" dy="${dy}" fill="${this.data.color}">${this.data.text}</text>
-        </svg>
-        `
-    }
-}
-
-
-class Triangle extends Shape {
-    constructor(_data){
-        super(_data);
-    }
-    render(){
-        return`<polygon points="50,5 95,95 5,95" stroke="black" stroke-width="1" fill="${this.data.background}" />`
-    }
-}
-
-class Square extends Shape {
-    constructor(_data){
-        super(_data);
-    }
-    render(){
-        return`<rect x="10" y="10" width="80" height="80" stroke="black" stroke-width="1" fill="${this.data.background}" />`
-    }
-}
-
-class Circle extends Shape {
-    constructor(_data){
-        super(_data);
-    }
-    render(){
-        return`  <circle cx="50" cy="50" r="40" stroke="black" stroke-width="1" fill="${this.data.background}" />`
-    }
-}
 function Start(){
+    const validateText = (input) => {
+        if (input.length <= 3) {
+          return true;
+        } else {
+          return 'Text input must be no more than 3 characters.';
+        }
+      };
+
     inquirer
     .prompt([
       {
@@ -65,6 +28,7 @@ function Start(){
         type: 'input',
         name: 'text',
         message: 'Enter the text input:',
+        validate: validateText,
       },
       {
         type: 'input',
@@ -78,6 +42,7 @@ function Start(){
 }
 
 function RenderShape(input){
+    console.log(input);
     const fileName = "./output/logo.svg"
     let newShape;
     switch(input.shape){
@@ -95,8 +60,6 @@ function RenderShape(input){
         if (err) console.error(err);
         else console.log(`File ${fileName} has been saved.`);
       });
-      
-
 }
 
 Start();
